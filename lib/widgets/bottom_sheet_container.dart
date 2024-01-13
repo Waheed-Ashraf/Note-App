2e170324492fc5aa11be1be7644ed04f6c8a9c7a
waheed_ashrf <washraf124@gmail.com>
2024-01-14 00:44:50 +0200

    adding bloc consumer to the bottom sheet to control the states of the addNoteState Cubit

--- lib/widgets/bottom_sheet_container.dart
+++ lib/widgets/bottom_sheet_container.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note_app/widgets/custom_bottom_sheet_form.dart';
 
 class BottomSheetContainer extends StatelessWidget {
@@ -15,9 +18,24 @@ class BottomSheetContainer extends StatelessWidget {
           ),
           color: Theme.of(context).colorScheme.primary,
         ),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: CustomBottomSheetForm(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: BlocConsumer<AddNoteCubit, AddNoteState>(
+            listener: (context, state) {
+              if (state is AddNoteFailure) {
+                print("there is somthing wrong ${state.errMessage}");
+              }
+              if (state is AddNoteSuccess) {
+                Navigator.pop(context);
+              }
+            },
+            builder: (context, state) {
+              return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoading ? true : false,
+                child: const CustomBottomSheetForm(),
+              );
+            },
+          ),
         ),
       ),
     );
